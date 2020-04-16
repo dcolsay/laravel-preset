58f7f7b7fbd5f9a8225f759ba18e5ebf4e50f689
Boris DEHOUMON <borisdehoumon@gmail.com>
2020-04-16 17:06:57 +0100

    update information

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dcolsay Laravel Preset
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Dcolevents</a>
+                    <a href="https://github.com/dcolsay">GitHub</a>
                 </div>
             </div>
         </div>
